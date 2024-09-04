dbcd9fdf1409920c40bb58306b7581f15195dac0
UserDebs <roumya2020second@gmail.com>
2024-09-04 15:16:35 +0530

    Adding Pickle file

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+from specilizationL import SpecializationTrie, Node
+
+import pickle as pkl
 import uvicorn
 
 app = FastAPI()
 
+workingDir = './Backend'
+specialization : SpecializationTrie = pkl.load(open('/'.join([workingDir, 'specilizations.pkl']), 'rb'))
+
 @app.get('/')
 async def root():
+    l : list[str] = specialization.search('sleep')
     return {
-        'written' : 'hello world'
+        'written' : l
     }
 
 if __name__ == '__main__':
