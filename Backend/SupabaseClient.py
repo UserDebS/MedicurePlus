4fca44d48b6e65b26ddcf5684be6589dd42019de
souvik <ghoshsouvik284@gmail.com>
2024-10-13 11:50:39 +0530

    1.0.2

--- Backend/SupabaseClient.py
+++ Backend/SupabaseClient.py
@@ -6,9 +6,10 @@ import time as t
 
 class Supabase:
     def __init__(self) -> None:
-        self.__key = getenv('SUPABASE_KEY')
-        self.__url = getenv('SUPABASE_URL')
+        self.__key ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJidnhtamdpenFydG5jbnV6Y3FwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzQwMTgsImV4cCI6MjA0MTAxMDAxOH0.TTkb7NVAUhV7xrnLsiCeGo-UqArncj1VeyBfgXwCIvQ' #getenv('SUPABASE_KEY')
+        self.__url = 'medicure@2024'#getenv('SUPABASE_URL')
         self.__instance : Client = create_client(self.__url, self.__key)
+    
     
     
     
--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,92 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from SupabaseClient import Supabase
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origin=['*'],
+    allow_crededentials=True,
+    allow_methods=['*'],
+    allow_headers=["Access-Control-Allow-Origin", 'Set-Cookie', 'Accept']
+)
+supabase = Supabase()
+
+class UserData(BaseModel):
+    username : str
+    password : str
+
+class MedicineDetails(BaseModel):
+    name : str
+    cost : float
+    quantity : int
+    self_url : str #http://localhost:3000/medicine/{id}
+
+@app.get('/login')
+def authByToken(req : Request, res : Response) -> dict[str, int]:
+    try:
+        token : str = req.cookies.get('medicure_auth')
+        supabase.authBytoken(token)
+    except:
+        return {
+            'status' : 404
+        }
+@app.post('/login')
+def authByUserPass(userdata : UserData, res : Response) -> dict[str, int]:
+    try:
+         #post userdata to database
+    except:
+        return{
+            'status':400
+        }
+@app.post('/register')
+def register(userdata : UserData, res : Response):
+    try:
+       #pass 'userdata' to supbase to register user
+    except:
+        return{
+            'status':404
+        }
+    
+@app.post('/medicine')
+def getMedicines() -> list[MedicineDetails]:
+    try:
+        
+    except:
+        return{
+            'status':400
+        }
+
+@app.get('/scan')
+def scan()->list[str]:
+    try:
+          #using Med we can get all medicine name
+    except:
+        return{
+            'status':400
+        }
+
+@app.get('/home')
+def homepage():
+    try:
+       #rendering homepage
+    except:
+        return{
+            'status':400
+        }
+
+@app.get('/home/')
+def suggestions(suggestion : str) -> list[str]:
+
+
+@app.post('/doctor')
+
+@app.post('/consult_doctor')
+
+@app.post('/delivery')
+
 
 if __name__ == '__main__':
     uvicorn.run(app, port=5500)
